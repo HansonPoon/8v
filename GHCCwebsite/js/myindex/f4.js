69047ec9b47de89a6209030e9cc45af59146e6ae
HansonPoon <429121246@qq.com>
2018-09-04 17:55:36 +0800

    手机上小标题未完

---
+++ GHCCwebsite/js/myindex/f4.js
@@ -0,0 +1,26 @@
+// 主页f4，右边hover显示左边图片
+// 01
+$("#f4_r_01").hover(function(){
+    $("#f4_l_01_img").css("opacity","1");
+
+},function(){
+    $("#f4_l_01_img").css("opacity","0");
+});
+// 02
+$("#f4_r_02").hover(function(){
+    $("#f4_l_02_img").css("opacity","1");
+},function(){
+    $("#f4_l_02_img").css("opacity","0");
+});
+// 03
+$("#f4_r_03").hover(function(){
+    $("#f4_l_03_img").css("opacity","1");
+},function(){
+    $("#f4_l_03_img").css("opacity","0");
+});
+// 04
+$("#f4_r_04").hover(function(){
+    $("#f4_l_04_img").css("opacity","1");
+},function(){
+    $("#f4_l_04_img").css("opacity","0");
+});
